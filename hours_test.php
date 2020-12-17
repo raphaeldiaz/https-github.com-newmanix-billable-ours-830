4f47dd68ac5ab0baffe866e98b167d9a828cdebb
raphaeldiaz <72459750+raphaeldiaz@users.noreply.github.com>
2020-12-17 08:29:54 -0800

    Update hours_test.php

--- hours_test.php
+++ hours_test.php
@@ -21,6 +21,21 @@ class LoginForm extends WebTestCase {
 You input 2 hours at a rate of $2 and your pay is $100
 	
 	you input 2 hours at a rate of $no and your pay is $0
-
+class HoursForm extends WebTestCase {
+	
+	function testCorrectData ( ) {
+		$this->get (VIRTUAL_PATH . ' /hours.php');
+		$this->assertResponse(200);
+		
+		$this->setField("hours", "2");
+		$this->setField("rate", "50");
+		$this->clickSubmit("Show Pay");
+		
+		$this->assertResponse(200);
+		$this->assertText("You input 2 hours at a rate of $50 and your pay is $100");
+		
+	}
+	
+}
 
 
